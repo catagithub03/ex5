55268ba685e5786eb99142e61a88a02c583d42a9
Bogdan.Savu <bogdan.savu@ro.ibm.com>
2020-03-03 11:19:04 +0200

    update php-helloworld application

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,8 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello IBM-SkillsAcademy World !!!<br />";
+print "The php version is " . PHP_VERSION . ".<br />";
+$VAR = getenv('VAR');
+if (getenv('VAR')) {
+    print "The environment variable VAR is $VAR !<br />";
+}
 ?>
