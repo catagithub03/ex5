bf2ea90a08e3b4762aa0ba1922594f57e96209ad
CONSTANTIN CATALIN NEAGOE <catalin.neagoe@ro.ibm.com>
2020-03-04 12:03:12 +0200

    clean up

--- php-helloworld/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-print "Hello IBM-SkillsAcademy World !!!<br />";
-print "The php version is " . PHP_VERSION . ".<br />";
-$VAR = getenv('VAR');
-if (getenv('VAR')) {
-    print "The environment variable VAR is $VAR !<br />";
-}
-?>
