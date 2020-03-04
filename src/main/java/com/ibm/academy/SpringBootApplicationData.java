e85259f24cb2ebc78057bda31574e073a65c7efb
Catalin N <catalin.neagoe@ro.ibm.com>
2020-03-04 13:26:03 +0200

    added swagger ui functionality

--- src/main/java/com/ibm/academy/SpringBootApplicationData.java
+++ src/main/java/com/ibm/academy/SpringBootApplicationData.java
@@ -4,8 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootApplicationData implements CommandLineRunner {
 
 	@Autowired
@@ -22,6 +31,14 @@ public class SpringBootApplicationData implements CommandLineRunner {
 
 		
 
+	}
+	
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.ibm.academy.controller")).paths(PathSelectors.any()).build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee Management API Skills Academy OpenShift")
+						.description("Employee Management API Skills Academy v1.0").build());
 	}
 
 }
